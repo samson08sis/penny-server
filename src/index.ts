aaae87ab66e59c2a5202984c4d2b7b1b347ef243
Samson <sams1307wolde@gmail.com>
2026-09-10 06:00:36 +0300

    Install and setup cookie parser & configure cors

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,24 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import routes from "./routes";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+const APP_URL = process.env.APP_URL;
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: APP_URL,
+    credentials: true,
+  })
+);
 
 app.use("/", routes);
 
